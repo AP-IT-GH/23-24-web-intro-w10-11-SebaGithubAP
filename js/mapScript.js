e19430b2870534cfc23b5ea45cbb40d65f278f4c
SebaGithubAP <150785157+SebaGithubAP@users.noreply.github.com>
2024-05-02 10:23:54 +0200

    start

---
+++ js/mapScript.js
@@ -0,0 +1,20 @@
+/* In dit script plaats je de code om de kaart in de id apMap te tekenen, zodat de AP-Hogeschool met adres Ellermanstraat 33 gecentreerd staat.  
+* Gebruik hiervoor de documentatie op https://leafletjs.com/ 
+* Verander in de code online alle "var" in "let".
+*/
+
+
+
+
+
+
+
+
+
+
+
+// bepaal de rechthoek rondom het gebouw van AP
+
+// kleur de rechthoek in met de rode AP-kleur
+
+// plaats een marker met als tekst "AP-Hogeschool" en eronder "Ellermanstraat 33"
